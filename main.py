a1ac1a85b48915bb55ae91f491a814720c5a8160
Barajoe <jtlmcgowan@gmail.com>
2025-10-28 16:28:30 +0000

    fixed the stupid counter

--- main.py
+++ main.py
@@ -9,25 +9,7 @@ book_text = get_book_text("books/frankenstein.txt")
 punctuation_remove = ["'", "-"]
 
 def char_count(book_text):
-    book_text = book_text.replace("\u00a0", " ")
-    book_text = (
-        book_text
-        .replace("‘", "'")
-        .replace("’", "'")
-        .replace("“", '"')
-        .replace("”", '"')
-        .replace("—", " ")
-        .replace("–", " ")
-    )
-    cleaned_text = ""
-    for char in book_text:
-        if char in string.punctuation:
-            if char in punctuation_remove:
-                char = ""
-            else:
-                char = " "
-        cleaned_text += char
-    words = cleaned_text.split()
+    words = book_text.split()
     return len(words)
 
 def main():
