f4597288ba368b24819124a9ed029f82ff019cce
Barajoe <jtlmcgowan@gmail.com>
2025-10-29 12:21:28 +0000

    sorting progress

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from stats import char_count
+from stats import word_count, char_count, sort_on, sorted_count
 
 def get_book_text(filepath):
     with open (filepath, encoding="utf-8") as f:
@@ -6,8 +6,10 @@ def get_book_text(filepath):
         return file_contents
 
 book_text = get_book_text("books/frankenstein.txt")
+count = char_count(book_text)
 
 def main():
-    print(f"Found {char_count(book_text)} total words")
+    print(f"Found {word_count(book_text)} total words")
+    print(sorted_count(char_count))
 
 main()
--- stats.py
+++ stats.py
@@ -1,3 +1,31 @@
-def char_count(book_text):
+def word_count(book_text):
     words = book_text.split()
     return len(words)
+
+def char_count(book_text):
+    chars = book_text.lower()
+    all_chars = dict()
+    for char in chars:
+        all_chars[char] = all_chars.get(char, 0) + 1
+    return all_chars
+
+def get_book_text(filepath):
+    with open (filepath, encoding="utf-8") as f:
+        file_contents = f.read()
+        return file_contents
+
+def sort_on(items):
+    return items["num"]
+
+book_text = get_book_text("books/frankenstein.txt")
+count = char_count(book_text)
+
+def sorted_count(count):
+    sorted = []
+    for char in count:
+        if char == str:
+            sorted.update({"char": char})
+        else:
+            sorted.update({"num": char})
+    sorted.sort(reverse=True, key=sort_on)
+    return sorted
